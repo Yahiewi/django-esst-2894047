38f452fed47490a9501283dab6ab1ab18acffee2
yahia <yahiahenien@gmail.com>
2025-03-08 15:55:02 +0100

    bitch

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+admin.site.register(models.Notes, NoteAdmin)
