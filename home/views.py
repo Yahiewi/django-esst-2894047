63b7c04993e118ee59693c39a49ff93443a8d27e
yahia <yahia.heni@imt-atlantique.net>
2025-03-08 12:45:40 +0100

    bitch

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Ahla bouzeb!")
+    # Here 'today' is a variable that we are passing to the template welcome.html
+    return render(request, 'home/welcome.html', {'today': datetime.today()})
