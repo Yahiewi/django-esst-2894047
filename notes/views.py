1e4ff3bffba4b550f9a08a648354ac7e6628d2f8
yahia <yahiahenien@gmail.com>
2025-03-09 14:42:40 +0100

    bitch

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Notes
+from django.http import Http404
 
 # Create your views here.
+def list(request):
+    all_notes = Notes.objects.all()
+    return render(request, 'notes/notes_list.html', {'notes': all_notes})
+
+def detail(request, pk):
+    try:
+        note = Notes.objects.get(pk=pk)
+    except Notes.DoesNotExist:
+        raise Http404("Note does not exist")
+    return render(request, 'notes/notes_detail.html', {'note': note})  
