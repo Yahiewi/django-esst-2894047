a61d66f5a3787daffc1c33ad5fee6a0b03a43b15
yahia <yahiahenien@gmail.com>
2025-03-09 17:29:53 +0100

    comments

--- notes/views.py
+++ notes/views.py
@@ -1,15 +1,35 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.views.generic import ListView, DetailView
 
 from .models import Notes
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = 'notes'
+    template_name = 'notes/notes_list.html'
 
-def detail(request, pk):
-    try: 
-        note = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-        raise Http404("This note doesn't exist")
-    return render(request, 'notes/notes_detail.html', {'note': note})
+# The exception handling is not needed in the class-based view because of DetailView
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = 'note'
+    template_name = 'notes/notes_detail.html'
+
+
+# class PopularNotesListView(ListView):
+#     model = Notes
+#     context_object_name = 'notes'
+#     template_name = 'notes/popular_notes_list.html'
+#     queryset = Notes.objects.filter(likes__gte=1)
+
+
+# def list(request):
+#     all_notes = Notes.objects.all()
+#     return render(request, 'notes/notes_list.html', {'notes': all_notes})
+
+# def detail(request, pk):
+#     try: 
+#         note = Notes.objects.get(pk=pk)
+#     except Notes.DoesNotExist:
+#         raise Http404("This note doesn't exist")
+#     return render(request, 'notes/notes_detail.html', {'note': note})
